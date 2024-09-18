b52c2eec542cd76345f04bf99820c452763483f1
ps <ps>
2024-09-18 14:18:58 +0200

    switch to docker v7

--- badi.py
+++ badi.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # Set up Chrome options
 chrome_options = Options()
